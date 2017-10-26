fff74ba55a39f0a443a22ba33fa7c0ef4f58f569
tangweikun <819573105@qq.com>
2017-10-26 13:41:04 +0800

    search insert

--- src/leetcode/SearchInsertPosition/index.js
+++ src/leetcode/SearchInsertPosition/index.js
@@ -1,4 +1,4 @@
-export function searchInsert(nums, target) {
+export function searchInsert1(nums, target) {
   const len = nums.length
   let i = 0
   while (i < len) {
@@ -7,4 +7,43 @@ export function searchInsert(nums, target) {
     i++
   }
   return len
+}
+
+export function searchInsert2(nums, target) {
+  const len = nums.length
+  let low = 0
+  let high = len - 1
+  while (low <= high) {
+    const mid = Math.floor((low + high) / 2)
+    if (target < nums[mid]) {
+      high = mid - 1
+    } else if (target > nums[mid]) {
+      low = mid + 1
+    } else return mid
+  }
+
+  return low
+}
+
+export function searchInsert(nums, target) {
+  if (target < nums[0]) return 0
+  if (target > nums[nums.length - 1]) return nums.length
+  return binarySearch(nums, target, 0, nums.length - 1)
+}
+
+function binarySearch(nums, target, low, high) {
+  const mid = low + Math.floor((high - low) / 2)
+  while (low + 1 < high) {
+    if (nums[mid] === target) {
+      return mid
+    } else if (nums[mid] > target) {
+      return binarySearch(nums, target, low, mid)
+    } else {
+      return binarySearch(nums, target, mid, high)
+    }
+  }
+
+  if (nums[low] === target) return low
+  if (nums[high === target]) return high
+  return mid + 1
 }
