10c0c02b66fe548045211f0dd0668411363d036e
tangweikun <819573105@qq.com>
2017-11-19 13:46:24 +0800

    :zap: shouldUpgradePro

--- src/interview/compareAppVersion/index.js
+++ src/interview/compareAppVersion/index.js
@@ -20,3 +20,17 @@ export function shouldUpgrade({ currentVersion, latestVersion }) {
 
   return false
 }
+
+export function shouldUpgradePro({ latestVersion, currentVersion }) {
+  const arr1 = latestVersion.split('.')
+  const arr2 = currentVersion.split('.')
+
+  while (arr1.length || arr2.length) {
+    const v1 = parseInt(arr1.shift()) || 0
+    const v2 = parseInt(arr2.shift()) || 0
+    if (v1 > v2) return true
+    if (v2 > v1) return false
+  }
+
+  return false
+}
