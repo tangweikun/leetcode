776f20c6faa23ad5485906617f4740f30aa328e2
tangweikun <819573105@qq.com>
2017-12-16 13:29:06 +0800

    :fire: Remove shit code

--- src/interview/compareAppVersion/index.js
+++ src/interview/compareAppVersion/index.js
@@ -1,27 +1,4 @@
-export function shouldUpgrade({ currentVersion, latestVersion }) {
-  const currentVersionElements = currentVersion.split('.')
-  const latestVersionElements = latestVersion.split('.')
-  const currentVersionElementsLength = currentVersionElements.length
-  const latestVersionElementsLength = latestVersionElements.length
-  let i = 0
-
-  while (i < currentVersionElementsLength) {
-    if (+currentVersionElements[i] < +latestVersionElements[i]) return true
-    if (+currentVersionElements[i] > +latestVersionElements[i]) return false
-    i++
-  }
-
-  if (currentVersionElementsLength < latestVersionElementsLength) {
-    while (i < latestVersionElementsLength) {
-      if (latestVersionElements[i] > 0) return true
-      i++
-    }
-  }
-
-  return false
-}
-
-export function shouldUpgradePro({ latestVersion, currentVersion }) {
+export function shouldUpgrade({ latestVersion, currentVersion }) {
   const arr1 = latestVersion.split('.')
   const arr2 = currentVersion.split('.')
 
