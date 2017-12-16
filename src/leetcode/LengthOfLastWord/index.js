ba84dd0f81c148f40e509d4e2fc31225120c8870
tangweikun <819573105@qq.com>
2017-12-16 16:44:18 +0800

    :sparkles: Length of Last Word Pro

--- src/leetcode/LengthOfLastWord/index.js
+++ src/leetcode/LengthOfLastWord/index.js
@@ -11,3 +11,14 @@ export function lengthOfLastWord(str) {
 
   return result
 }
+
+export function lengthOfLastWordPro(str) {
+  const arr = str.split(' ')
+  let position = arr.length - 1
+
+  while (!arr[position] && position > 0) {
+    position -= 1
+  }
+
+  return arr[position].length
+}
