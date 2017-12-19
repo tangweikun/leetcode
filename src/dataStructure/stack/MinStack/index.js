d4cfca80f7dceea56c5be7847058390652256804
tangweikun <819573105@qq.com>
2017-12-19 13:01:47 +0800

    :sparkles: MinStack

---
+++ src/dataStructure/stack/MinStack/index.js
@@ -0,0 +1,15 @@
+var MinStack = function() {
+  this.stack = []
+  this.pop = () => this.stack.pop()
+  this.top = () =>
+    this.stack[this.stack.length - 1] && this.stack[this.stack.length - 1].val
+  this.getMin = () =>
+    this.stack[this.stack.length - 1] && this.stack[this.stack.length - 1].min
+  this.push = val => {
+    const min =
+      this.stack.length > 0
+        ? this.stack[this.stack.length - 1].min
+        : Number.MAX_SAFE_INTEGER
+    this.stack.push({ val, min: Math.min(min, val) })
+  }
+}
