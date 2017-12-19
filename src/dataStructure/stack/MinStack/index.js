4a0ce96fcf88f1f26975ccb70506dd12e0cfeeac
tangweikun <819573105@qq.com>
2017-12-19 19:27:31 +0800

    :art: Format of MinStack code

--- src/dataStructure/stack/MinStack/index.js
+++ src/dataStructure/stack/MinStack/index.js
@@ -1,10 +1,14 @@
 var MinStack = function() {
   this.stack = []
+
   this.pop = () => this.stack.pop()
+
   this.top = () =>
     this.stack[this.stack.length - 1] && this.stack[this.stack.length - 1].val
+
   this.getMin = () =>
     this.stack[this.stack.length - 1] && this.stack[this.stack.length - 1].min
+
   this.push = val => {
     const min =
       this.stack.length > 0
