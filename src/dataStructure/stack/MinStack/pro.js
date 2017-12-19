25568e6f9d8f1116a977038b2e1f19640a693d00
tangweikun <819573105@qq.com>
2017-12-19 19:40:31 +0800

    :zap: Add MinStackPro

---
+++ src/dataStructure/stack/MinStack/pro.js
@@ -0,0 +1,26 @@
+MinStackPro = function() {
+  this.stack = []
+  this.assistStack = []
+
+  this.pop = () => {
+    const { assistStack, stack } = this
+    if (assistStack[assistStack.length - 1] === stack.length - 1) {
+      assistStack.pop()
+    }
+    this.stack.pop()
+  }
+
+  this.top = () => this.stack[this.stack.length - 1]
+
+  this.getMin = () => this.stack[this.assistStack[this.assistStack.length - 1]]
+
+  this.push = value => {
+    const { stack, assistStack } = this
+    const min = stack[assistStack[assistStack.length - 1]]
+
+    if (!assistStack.length || value < min) {
+      assistStack.push(stack.length)
+    }
+    stack.push(value)
+  }
+}
