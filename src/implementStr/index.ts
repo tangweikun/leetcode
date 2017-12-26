b8cb0ff1aedd84d88fcb25a81b68219f62a04b07
tangweikun <819573105@qq.com>
2017-12-26 18:53:00 +0800

    :art: Format code

--- src/implementStr/index.ts
+++ src/implementStr/index.ts
@@ -1,6 +1,7 @@
 export function strStr(haystack: string, needle: string): number {
   const haystackLen = haystack.length
   const needleLen = needle.length
+
   if (needle === '') return 0
   for (let i = 0; i < haystackLen; i++) {
     let isExist = true
@@ -13,5 +14,6 @@ export function strStr(haystack: string, needle: string): number {
     if (isExist) return i
     if (needleLen + i >= haystackLen) return -1
   }
+
   return -1
 }
