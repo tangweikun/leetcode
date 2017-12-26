c89360a4ab9e928cb8c6b2a70d794ebafaf4bf62
tangweikun <819573105@qq.com>
2017-12-26 18:40:13 +0800

    :hammer: Remove unnecessary if

--- src/implementStr/index.ts
+++ src/implementStr/index.ts
@@ -3,14 +3,11 @@ export function strStr(haystack: string, needle: string): number {
   const needleLen = needle.length
   if (needle === '') return 0
   for (let i = 0; i < haystackLen; i++) {
-    let isExist = false
-    if (haystack[i] === needle[0]) {
-      isExist = true
-      for (let j = 1; j < needleLen; j++) {
-        if (haystack[i + j] !== needle[j]) {
-          isExist = false
-          break
-        }
+    let isExist = true
+    for (let j = 0; j < needleLen; j++) {
+      if (haystack[i + j] !== needle[j]) {
+        isExist = false
+        break
       }
     }
     if (isExist) return i
