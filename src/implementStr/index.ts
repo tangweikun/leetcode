e83e8b49da7189596534e0089a6ff54adbc84dc3
tangweikun <819573105@qq.com>
2017-12-27 20:59:58 +0800

    :hammer: Remove unnecessary flag isExist

--- src/implementStr/index.ts
+++ src/implementStr/index.ts
@@ -4,14 +4,13 @@ export function strStr(haystack: string, needle: string): number {
 
   if (needle === '') return 0
   for (let i = 0; i < haystackLen; i++) {
-    let isExist = true
     for (let j = 0; j < needleLen; j++) {
       if (haystack[i + j] !== needle[j]) {
-        isExist = false
         break
       }
+      if (j === needleLen - 1) return i
     }
-    if (isExist) return i
+
     if (needleLen + i >= haystackLen) return -1
   }
 
