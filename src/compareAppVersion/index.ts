c20aa3d72a0538a2682489cdde7b52173c44c24f
tangweikun <819573105@qq.com>
2017-12-28 00:06:09 +0800

    :hammer: Lower complexity

--- src/compareAppVersion/index.ts
+++ src/compareAppVersion/index.ts
@@ -1,13 +1,11 @@
-// FIXME: Write right typeCheck
-export function shouldUpgrade({ latestVersion, currentVersion }: any) {
+export function shouldUpgrade({ latestVersion, currentVersion }: any): boolean {
   const arr1 = latestVersion.split('.')
   const arr2 = currentVersion.split('.')
 
-  while (arr1.length || arr2.length) {
+  while (arr1.length + arr2.length > 0) {
     const v1 = parseInt(arr1.shift()) || 0
     const v2 = parseInt(arr2.shift()) || 0
-    if (v1 > v2) return true
-    if (v2 > v1) return false
+    if (v1 !== v2) return v1 > v2
   }
 
   return false
