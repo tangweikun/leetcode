f64bf570d27e949ca7405f777b538ac41a7fd7de
tangweikun <819573105@qq.com>
2017-12-29 13:07:13 +0800

    :hammer: Use Math.max to make code readable

--- src/maximumSubArray/index.ts
+++ src/maximumSubArray/index.ts
@@ -1,11 +1,11 @@
 export function maximumSubArray(nums: number[]) {
-  let preSum = 0
+  let currentSum = 0
   let maxSum = -Infinity
 
   for (let elem of nums) {
-    const nextSum = preSum + elem
-    if (nextSum > maxSum) maxSum = nextSum
-    preSum = nextSum > 0 ? nextSum : 0
+    const nextSum = currentSum + elem
+    maxSum = Math.max(maxSum, nextSum)
+    currentSum = Math.max(nextSum, 0)
   }
 
   return maxSum
