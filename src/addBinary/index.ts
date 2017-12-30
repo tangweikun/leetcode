af59fe19b2c322b33308b3bc54905bf6be576483
tangweikun <819573105@qq.com>
2017-12-31 01:29:40 +0800

    :hammer: Refactory add binary

--- src/addBinary/index.ts
+++ src/addBinary/index.ts
@@ -3,21 +3,13 @@ export function addBinary(a: string, b: string): string {
   let temp = 0
   let i = a.length - 1
   let j = b.length - 1
+  const getNumber = (str: string) => (str === undefined ? 0 : Number(str))
 
-  while (i >= 0 || j >= 0) {
-    const NumberA = a[i] === undefined ? 0 : Number(a[i])
-    const NumberB = b[j] === undefined ? 0 : Number(b[j])
-    const currentSum = NumberA + NumberB + temp
-    if (currentSum >= 2) {
-      result = currentSum - 2 + result
-      temp = 1
-    } else {
-      result = currentSum + result
-      temp = 0
-    }
-    i--
-    j--
+  while (i >= 0 || j >= 0 || temp === 1) {
+    const currentSum = getNumber(a[i--]) + getNumber(b[j--]) + temp
+    result = currentSum % 2 + result
+    temp = currentSum >> 1
   }
 
-  return temp ? temp + result : result
+  return result
 }
