a3971bf69921d58c7e92584cd5644c785c35de37
tangweikun <819573105@qq.com>
2018-01-01 17:15:42 +0800

    :white_check_mark: Add test case for merge sorted array

--- src/mergeSortedArray/test.ts
+++ src/mergeSortedArray/test.ts
@@ -1,4 +1,4 @@
-import { mergeSortedArray } from '.'
+import { mergeSortedArray } from './pro'
 
 test('mergeSortedArray-1', () => {
   expect(mergeSortedArray([1, 2, 4, 8], [2, 7, 8])).toEqual([1, 2, 2, 4, 7, 8, 8])
@@ -10,5 +10,13 @@ test('mergeSortedArray-2', () => {
 
 test('mergeSortedArray-3', () => {
   expect(mergeSortedArray([], [0])).toEqual([0])
+})
+
+test('mergeSortedArray-4', () => {
+  expect(mergeSortedArray([0], [1])).toEqual([0, 1])
+})
+
+test('mergeSortedArray-5', () => {
+  expect(mergeSortedArray([2, 3], [1])).toEqual([1, 2, 3])
 })
 
