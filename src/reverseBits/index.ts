a74f9386cf153ef54111d18d79ab7785b57d5c58
tangweikun <819573105@qq.com>
2018-01-08 14:57:42 +0800

    :zap: Reduce time complexity

--- src/reverseBits/index.ts
+++ src/reverseBits/index.ts
@@ -1,16 +1,13 @@
 export function reverseBits(num: number) {
   let result = 0
   let bit = ''
+  let i = 0
 
   while (num >= 1) {
     bit += num % 2
     num = num % 2 === 1 ? (num - 1) / 2 : num / 2
-  }
-
-  const distinct = 32 - bit.length
-
-  for (let i = 0; i < bit.length; i++) {
-    result += Math.pow(2, bit.length - 1 - i + distinct) * bit[i]
+    result += Math.pow(2, 31 - i) * bit[i]
+    i++
   }
 
   return result
