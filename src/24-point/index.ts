713f0c9c81b4dc33db71c206ff717ec998d05d5c
tangweikun <819573105@qq.com>
2018-01-09 23:02:32 +0800

    :shit: 24 Point

---
+++ src/24-point/index.ts
@@ -0,0 +1,75 @@
+export function isConform(arr: any[]) {
+  const operatorStack = []
+  const numberStack = []
+  const arr2 = []
+  const helpArr2 = []
+
+  for (let i = 0; i < arr.length; i++) {
+    if (!isNaN(arr[i])) {
+      arr2.push(arr[i])
+    } else {
+      if (arr[i] === '(') {
+        helpArr2.push(arr[i])
+      } else if (arr[i] === ')') {
+        while (helpArr2[helpArr2.length - 1] !== '(') {
+          arr2.push(helpArr2[helpArr2.length - 1])
+          helpArr2.pop()
+        }
+        helpArr2.pop()
+      } else if (priority[arr[i]] <= priority[helpArr2[helpArr2.length - 1]]) {
+        while (helpArr2[helpArr2.length - 1] !== '(' && helpArr2.length) {
+          arr2.push(helpArr2[helpArr2.length - 1])
+          helpArr2.pop()
+        }
+        helpArr2.push(arr[i])
+      } else {
+        helpArr2.push(arr[i])
+      }
+    }
+  }
+
+  const ans = [...arr2, ...helpArr2.reverse()]
+  const stack = []
+  for (let k = 0; k < ans.length; k++) {
+    if (isNaN(ans[k])) {
+      const res = calculate(
+        stack[stack.length - 2],
+        stack[stack.length - 1],
+        ans[k],
+      )
+      stack.pop()
+      stack.pop()
+      stack.push(res)
+    } else {
+      stack.push(ans[k])
+    }
+  }
+
+  return stack[0] === 24
+}
+
+const priority = {
+  '+': 0,
+  '-': 0,
+  '*': 1,
+  '/': 1,
+}
+
+const calculate = (num1: number, num2: number, operator: string) => {
+  switch (operator) {
+    case '+':
+      return num1 + num2
+
+    case '-':
+      return num1 - num2
+
+    case '*':
+      return num1 * num2
+
+    case '/':
+      return num1 / num2
+
+    default:
+      return 0
+  }
+}
