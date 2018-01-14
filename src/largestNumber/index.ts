f5c46dfb0f3f7f2746663b65a7d2e7d4aee0fb4b
tangweikun <819573105@qq.com>
2018-01-14 17:19:06 +0800

    :hammer: Reduce complexity for largestNumber

--- src/largestNumber/index.ts
+++ src/largestNumber/index.ts
@@ -1,21 +1,28 @@
-// TODO:
+// TODO: Type check
 
 export function largestNumber(nums: number[]) {
   let max = { pos: -1, value: -Infinity }
   let i = 0
 
   while (i < nums.length) {
-    if (max.value < nums[i]) {
-      max = {
-        pos: max.value * 2 <= nums[i] ? i : -1,
-        value: nums[i],
-      }
-    } else if (max.value < nums[i] * 2) {
-      max.pos = -1
-    }
-
+    const currentValue = nums[i]
+    max = modifyMax(max, currentValue, i)
     i++
   }
 
   return max.pos
+}
+
+const modifyMax = (max, currentValue: number, index: number) => {
+  const maxValue = max.value
+  if (maxValue < currentValue) {
+    max = {
+      pos: maxValue * 2 <= currentValue ? index : -1,
+      value: currentValue,
+    }
+  } else if (maxValue < currentValue * 2) {
+    max.pos = -1
+  }
+
+  return max
 }
