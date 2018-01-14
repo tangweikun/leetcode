1397a6e0edb840c946a0cda0fe434323b986618b
tangweikun <819573105@qq.com>
2018-01-14 19:42:46 +0800

    :memo: Add Complexity comment

--- src/hammingWeight/index.ts
+++ src/hammingWeight/index.ts
@@ -1,7 +1,10 @@
+// Time Complexity O(1)
+// Space Complexity O(1)
+
 export function hammingWeight(num: number) {
   let sum = 0
 
-  while (num > 0) {
+  while (num !== 0) {
     if (num & 1) sum += 1
     num >>>= 1
   }
