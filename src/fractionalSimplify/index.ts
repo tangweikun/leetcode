e5f5878bc1bb65e0ba9ba13edef77d0083ad7a3d
tangweikun <819573105@qq.com>
2018-01-14 12:23:27 +0800

    :hammer: Abstract constant

--- src/fractionalSimplify/index.ts
+++ src/fractionalSimplify/index.ts
@@ -1,24 +1,24 @@
 import { greatestCommonDivisor } from '../greatestCommonDivisor'
 
 export function fractionalSimplify(numerator: number, denominator: number) {
-  if (numerator === 0) return [0]
+  if (numerator === 0 || denominator === 0) return [0]
 
   const absNumerator = Math.abs(numerator)
   const absDenominator = Math.abs(denominator)
-  const sign = Math.sign(numerator) * Math.sign(denominator)
+  const SIGN = Math.sign(numerator) * Math.sign(denominator)
   const REMINDER = absNumerator % absDenominator
+  const QUOTIENT = absNumerator / absDenominator
 
-  if (REMINDER === 0) return [sign * absNumerator / absDenominator]
+  if (REMINDER === 0) return [SIGN * QUOTIENT]
 
   const GREATEST_COMMON_DIVISOR = greatestCommonDivisor(
     absNumerator,
     absDenominator,
   )
-  const QUOTIENT = Math.floor(absNumerator / absDenominator)
 
   return [
-    sign * QUOTIENT || 0,
-    sign * (REMINDER / GREATEST_COMMON_DIVISOR),
+    SIGN * Math.floor(QUOTIENT) || 0,
+    SIGN * (REMINDER / GREATEST_COMMON_DIVISOR),
     absDenominator / GREATEST_COMMON_DIVISOR,
   ]
 }
