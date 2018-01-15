931a5f3a9eecc44ca2e4e1af1dd3bd7a642d7d79
tangweikun <819573105@qq.com>
2018-01-15 22:48:45 +0800

    :hammer: Remove unnecessary condition

--- src/islandPerimeter/index.ts
+++ src/islandPerimeter/index.ts
@@ -1,11 +1,9 @@
 export function islandPerimeter(grid: [number][]) {
   let sum = 0
-  const width = grid[0].length
-  const height = grid.length
 
-  for (let h = 0; h < height; h++) {
-    for (let w = 0; w < width; w++) {
-      if (grid[h] && grid[h][w]) {
+  for (let h = 0; h < grid.length; h++) {
+    for (let w = 0; w < grid[h].length; w++) {
+      if (grid[h][w]) {
         sum += 4
         if (grid[h][w + 1]) {
           sum -= 2
