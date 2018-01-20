1f0f3c018d6e1f42993b5eccf1552916ba3ad751
tangweikun <819573105@qq.com>
2018-01-20 13:45:31 +0800

    :hammer: Rename reminer --> carry

--- src/addStrings/index.ts
+++ src/addStrings/index.ts
@@ -1,14 +1,13 @@
 export function addStrings(num1: string, num2: string) {
   let [i, j] = [num1.length - 1, num2.length - 1]
   let ans = ''
-  let reminder = 0
+  let carry = 0
 
-  for (; i >= 0 || j >= 0; i--, j--) {
-    const sum = ~~num1[i] + ~~num2[j] + reminder
+  for (; i >= 0 || j >= 0 || carry === 1; i--, j--) {
+    const sum = ~~num1[i] + ~~num2[j] + carry
     ans = sum % 10 + ans
-    reminder = ~~(sum / 10)
+    carry = ~~(sum / 10)
   }
-  reminder && (ans = reminder + ans)
 
   return ans
 }
