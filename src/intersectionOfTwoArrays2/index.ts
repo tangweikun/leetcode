c096c91b13940d7fad2a5d83bf82a93f0c76125f
tangweikun <819573105@qq.com>
2018-01-22 01:47:12 +0800

    :hammer: Reduce complexity

--- src/intersectionOfTwoArrays2/index.ts
+++ src/intersectionOfTwoArrays2/index.ts
@@ -6,7 +6,7 @@ export function intersectionOfTwoArrays2(arr1: number[], arr2: number[]) {
   const ans = []
 
   for (let num of arr1) {
-    hash[num] = hash[num] ? hash[num] + 1 : 1
+    hash[num] = ~~hash[num] + 1
   }
 
   for (let num of arr2) {
