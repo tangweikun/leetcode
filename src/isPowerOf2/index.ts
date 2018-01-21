af65adb10392f2bcc8fbd7b5a541c7b4d691fe9f
tangweikun <819573105@qq.com>
2018-01-21 15:30:12 +0800

    Revert ":bug: Fix bug when num === 0"
    This reverts commit b99ab7d214b1f08f5811551955a51be5cb321f51.

--- src/isPowerOf2/index.ts
+++ src/isPowerOf2/index.ts
@@ -1,6 +1,5 @@
 // Time Complexity O(log N)
 export function isPowerOf2(num: number): boolean {
-  if (num === 0) return false
   if (num === 1) return true
   if (num % 2) return false
   return isPowerOf2(num / 2)
