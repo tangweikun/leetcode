00bfe483acaaa43d42ec2d3ba0795acf58185a5b
tangweikun <819573105@qq.com>
2018-01-31 09:55:47 +0800

    :hammer: Refactor minimum index sum of two lists

--- src/minimumIndexSumOfTwoLists/index.ts
+++ src/minimumIndexSumOfTwoLists/index.ts
@@ -1,31 +1,37 @@
 export function findRestaurant(arr1: string[], arr2: string[]) {
-  const hash = new Map()
-  const temp = new Map()
   let res: string[] = []
   let min = Infinity
+  const commonRestaurant = getCommonRestaurant(arr1, arr2)
 
-  for (let i = 0; i < Math.max(arr1.length, arr2.length); i++) {
-    if (hash.has(arr1[i]) || hash.has(arr2[i])) {
-      hash.has(arr1[i]) && temp.set(arr1[i], i + hash.get(arr1[i]))
-      hash.has(arr2[i]) && temp.set(arr2[i], i + hash.get(arr2[i]))
-    } else {
-      if (arr1[i] === arr2[i]) {
-        temp.set(arr1[i], i * 2)
-      } else {
-        arr1[i] && hash.set(arr1[i], i)
-        arr2[i] && hash.set(arr2[i], i)
-      }
+  commonRestaurant.forEach((value, key) => {
+    if (value === min) {
+      res.push(key)
     }
-  }
-
-  temp.forEach((value, key) => {
     if (value < min) {
       res = [key]
       min = value
-    } else if (value === min) {
-      res.push(key)
     }
   })
 
   return res
+}
+
+const getCommonRestaurant = (arr1: string[], arr2: string[]) => {
+  const hash = new Map()
+  const temp = new Map()
+  for (let i = 0; i < Math.max(arr1.length, arr2.length); i++) {
+    if (hash.has(arr1[i]) || hash.has(arr2[i])) {
+      temp.set(arr1[i], i + hash.get(arr1[i]))
+      temp.set(arr2[i], i + hash.get(arr2[i]))
+    } else {
+      if (arr1[i] === arr2[i]) {
+        temp.set(arr1[i], i * 2)
+      } else {
+        hash.set(arr1[i], i)
+        hash.set(arr2[i], i)
+      }
+    }
+  }
+
+  return temp
 }
