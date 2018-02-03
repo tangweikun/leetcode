da3cfe0b2241cfcd68422c453cd0bd0292d4ed4f
tangweikun <819573105@qq.com>
2018-02-03 15:39:03 +0800

    :hammer: Refactor valid sqaure

--- src/validSquare/index.ts
+++ src/validSquare/index.ts
@@ -4,12 +4,12 @@ export function validSquare(
   p3: number[],
   p4: number[],
 ) {
-  const p12Square = Math.pow(p1[0] - p2[0], 2) + Math.pow(p1[1] - p2[1], 2)
-  const p23Square = Math.pow(p2[0] - p3[0], 2) + Math.pow(p2[1] - p3[1], 2)
-  const p34Square = Math.pow(p3[0] - p4[0], 2) + Math.pow(p3[1] - p4[1], 2)
-  const p41Square = Math.pow(p4[0] - p1[0], 2) + Math.pow(p4[1] - p1[1], 2)
-  const p13Square = Math.pow(p1[0] - p3[0], 2) + Math.pow(p1[1] - p3[1], 2)
-  const p24Square = Math.pow(p2[0] - p4[0], 2) + Math.pow(p2[1] - p4[1], 2)
+  const p12Square = getDistince(p1, p2)
+  const p23Square = getDistince(p2, p3)
+  const p34Square = getDistince(p3, p4)
+  const p41Square = getDistince(p4, p1)
+  const p13Square = getDistince(p1, p3)
+  const p24Square = getDistince(p2, p4)
 
   const foo = [p12Square, p23Square, p34Square, p41Square, p13Square, p24Square]
   const arr1 = [p12Square]
@@ -29,3 +29,6 @@ export function validSquare(
   // 4 * 2 = 8
   return arr1.length * arr2.length === 8
 }
+
+const getDistince = (a: number[], b: number[]) =>
+  Math.pow(a[0] - b[0], 2) + Math.pow(a[1] - b[1], 2)
