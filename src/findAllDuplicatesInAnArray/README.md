22ed1e47593c62a4f4bec997f66b3d5d37a13769
tangweikun <819573105@qq.com>
2018-02-07 20:51:55 +0800

    :sparkles: Find all Duplicates in an Array

---
+++ src/findAllDuplicatesInAnArray/README.md
@@ -0,0 +1,18 @@
+# 115.Find All Duplicates in an Array
+
+## Description
+
+Given an array of integers, 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+
+Find all the elements that appear twice in this array.
+
+## Example
+
+```javascript
+Input: [4, 3, 2, 7, 8, 2, 3, 1]
+Output: [2, 3]
+```
+
+## From
+
+[LeetCode](https://leetcode.com/problems/find-all-duplicates-in-an-array)
---
+++ src/findAllDuplicatesInAnArray/test.ts
@@ -0,0 +1,17 @@
+import { findDuplicates } from '.'
+
+test('FindAllDuplicatesInAnArray-1', () => {
+  expect(findDuplicates([4, 3, 2, 7, 8, 2, 3, 1]).sort()).toEqual([2, 3])
+})
+
+test('FindAllDuplicatesInAnArray-1', () => {
+  expect(findDuplicates([3, 3, 1]).sort()).toEqual([3])
+})
+
+test('FindAllDuplicatesInAnArray-1', () => {
+  expect(findDuplicates([1, 4, 4, 1]).sort()).toEqual([1, 4])
+})
+
+test('FindAllDuplicatesInAnArray-1', () => {
+  expect(findDuplicates([1, 3, 4, 2]).sort()).toEqual([])
+})
