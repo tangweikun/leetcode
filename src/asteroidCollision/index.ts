d016f2f32f11b67c5ba48dad922baf0a08639ff6
tangweikun <819573105@qq.com>
2018-02-11 23:08:07 +0800

    :hammer: Reduce complexity of asteroid collision

--- src/asteroidCollision/index.ts
+++ src/asteroidCollision/index.ts
@@ -1,24 +1,16 @@
 import { _peek } from '../_utils'
 
 export function asteroidCollision(asteroids: number[]) {
-  const stack: any = []
-  for (let i = 0; i < asteroids.length; i++) {
-    if (~~_peek(stack) <= 0) {
-      stack.push(asteroids[i])
-    } else {
-      if (asteroids[i] > 0) stack.push(asteroids[i])
-      else {
-        while (_peek(stack) > 0 && _peek(stack) + asteroids[i] < 0) {
-          stack.pop()
-        }
-        if (_peek(stack) + asteroids[i] === 0) {
-          stack.pop()
-        } else if (~~_peek(stack) <= 0) {
-          stack.push(asteroids[i])
-        }
-      }
+  const res = []
+
+  for (var i = 0; i < asteroids.length; i++) {
+    if (asteroids[i] > 0 || ~~_peek(res) <= 0) {
+      res.push(asteroids[i])
+    } else if (_peek(res) + asteroids[i] <= 0) {
+      if (_peek(res) + asteroids[i] < 0) i--
+      res.pop()
     }
   }
 
-  return stack
+  return res
 }
--- src/asteroidCollision/test.ts
+++ src/asteroidCollision/test.ts
@@ -4,22 +4,26 @@ test('AsteroidCollision-1', () => {
   expect(asteroidCollision([5, 10, -5])).toEqual([5, 10])
 })
 
-test('AsteroidCollision-1', () => {
+test('AsteroidCollision-2', () => {
   expect(asteroidCollision([8, -8])).toEqual([])
 })
 
-test('AsteroidCollision-1', () => {
+test('AsteroidCollision-3', () => {
   expect(asteroidCollision([10, 2, -5])).toEqual([10])
 })
 
-test('AsteroidCollision-1', () => {
+test('AsteroidCollision-4', () => {
   expect(asteroidCollision([-2, -1, 1, 2])).toEqual([-2, -1, 1, 2])
 })
 
-test('AsteroidCollision-1', () => {
+test('AsteroidCollision-5', () => {
   expect(asteroidCollision([-2, -1, 1, 2, -3, 4])).toEqual([-2, -1, -3, 4])
 })
 
-test('AsteroidCollision-1', () => {
+test('AsteroidCollision-6', () => {
   expect(asteroidCollision([1, -2, -2, -2])).toEqual([-2, -2, -2])
+})
+
+test('AsteroidCollision-7', () => {
+  expect(asteroidCollision([1, 2, 4, 3, -5])).toEqual([-5])
 })
