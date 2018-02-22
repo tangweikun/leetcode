7a792ac1529a5ae60d09250e41da6817e15ba0ba
tangweikun <819573105@qq.com>
2018-02-22 17:42:41 +0800

    :zap: Reduce complexity of validSquare

--- src/validSquare/index.ts
+++ src/validSquare/index.ts
@@ -4,31 +4,28 @@ export function validSquare(
   p3: number[],
   p4: number[],
 ) {
-  const p12Square = getDistince(p1, p2)
-  const p23Square = getDistince(p2, p3)
-  const p34Square = getDistince(p3, p4)
-  const p41Square = getDistince(p4, p1)
-  const p13Square = getDistince(p1, p3)
-  const p24Square = getDistince(p2, p4)
+  const p12Square = getDistance(p1, p2)
+  const p23Square = getDistance(p2, p3)
+  const p34Square = getDistance(p3, p4)
+  const p41Square = getDistance(p4, p1)
+  const p13Square = getDistance(p1, p3)
+  const p24Square = getDistance(p2, p4)
 
-  const foo = [p12Square, p23Square, p34Square, p41Square, p13Square, p24Square]
-  const arr1 = [p12Square]
-  const arr2 = []
+  const lines = [
+    p12Square,
+    p23Square,
+    p34Square,
+    p41Square,
+    p13Square,
+    p24Square,
+  ].sort((x, y) => x - y)
 
-  for (let i = 1; i < 6; i++) {
-    if (arr1[0] === foo[i]) {
-      arr1.push(foo[i])
-    } else {
-      if (arr2.length > 0 && arr2[0] !== foo[i]) {
-        return false
-      }
-      arr2.push(foo[i])
-    }
-  }
-
-  // 4 * 2 = 8
-  return arr1.length * arr2.length === 8
+  return (
+    lines[0] + lines[1] === lines[2] + lines[3] &&
+    lines[4] === lines[5] &&
+    lines[4] > lines[3]
+  )
 }
 
-const getDistince = (a: number[], b: number[]) =>
+const getDistance = (a: number[], b: number[]) =>
   Math.pow(a[0] - b[0], 2) + Math.pow(a[1] - b[1], 2)
--- src/validSquare/test.ts
+++ src/validSquare/test.ts
@@ -4,14 +4,20 @@ test('validSquare-1', () => {
   expect(validSquare([0, 0], [1, 1], [1, 0], [0, 1])).toBe(true)
 })
 
-test('validSquare-1', () => {
+test('validSquare-2', () => {
   expect(validSquare([2, 2], [2, 4], [4, 2], [4, 4])).toBe(true)
 })
 
-test('validSquare-1', () => {
+test('validSquare-3', () => {
   expect(validSquare([2, 2], [2, 4], [5, 2], [5, 4])).toBe(false)
 })
 
-test('validSquare-1', () => {
+test('validSquare-4', () => {
   expect(validSquare([0, 0], [0, 0], [0, 0], [0, 0])).toBe(false)
+})
+
+test('validSquare-5', () => {
+  expect(
+    validSquare([4604, 1524], [4574, 2274], [5354, 1554], [5324, 2304]),
+  ).toBe(true)
 })
