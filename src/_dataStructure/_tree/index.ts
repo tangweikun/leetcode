59e5ca6b326825cf91d01430d934936d4d9c5fa1
tangweikun <819573105@qq.com>
2018-02-24 15:07:44 +0800

    :construction: Push of BinarySearchTree

--- src/_dataStructure/_tree/index.ts
+++ src/_dataStructure/_tree/index.ts
@@ -5,4 +5,34 @@ export default class BinarySearchTree {
   constructor() {
     this.root = null
   }
+
+  push(value: any) {
+    const root = this.root
+
+    if (!root) {
+      this.root = new Node(value)
+      return
+    }
+
+    let currentNode = root
+    const newNode = new Node(value)
+
+    while (currentNode) {
+      if (value < currentNode.value) {
+        if (!currentNode.left) {
+          currentNode.left = newNode
+          break
+        } else {
+          currentNode = currentNode.left
+        }
+      } else {
+        if (!currentNode.right) {
+          currentNode.right = newNode
+          break
+        } else {
+          currentNode = currentNode.right
+        }
+      }
+    }
+  }
 }
--- src/_dataStructure/_tree/test.ts
+++ src/_dataStructure/_tree/test.ts
@@ -6,3 +6,29 @@ describe('BinarySearchTree-Create', () => {
     expect(tree1).toEqual({ root: null })
   })
 })
+
+describe('BinarySearchTree-Push', () => {
+  const tree2 = new BinarySearchTree()
+  tree2.push(3)
+  it('-->1', () => {
+    expect(tree2).toEqual({ root: { data: 3, left: null, right: null } })
+  })
+
+  const tree3 = new BinarySearchTree()
+  tree3.push(1)
+  tree3.push(2)
+  tree3.push(3)
+  it('-->2', () => {
+    expect(tree3).toEqual({
+      root: {
+        data: 1,
+        left: null,
+        right: {
+          data: 2,
+          left: null,
+          right: { data: 3, left: null, right: null },
+        },
+      },
+    })
+  })
+})
