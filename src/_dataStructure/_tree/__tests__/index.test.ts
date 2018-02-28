c3ad60b7aa2862e53457156b9f783b5a8b81a7be
tangweikun <819573105@qq.com>
2018-02-28 10:39:52 +0800

    :construction: Depth-First Search

--- src/_dataStructure/_tree/__tests__/index.test.ts
+++ src/_dataStructure/_tree/__tests__/index.test.ts
@@ -1,6 +1,41 @@
 import Tree from '..'
+import { TreeNode } from '../treeNode'
 
 test('tree-1', () => {
   const tree1 = new Tree('CEO')
   expect(tree1._root).toEqual({ val: 'CEO', parent: null, children: [] })
+})
+
+test('tree-2', () => {
+  const tree = new Tree('one')
+  tree._root.children.push(new TreeNode('two'))
+  tree._root.children[0].parent = tree
+
+  tree._root.children.push(new TreeNode('three'))
+  tree._root.children[1].parent = tree
+
+  tree._root.children.push(new TreeNode('four'))
+  tree._root.children[2].parent = tree
+
+  tree._root.children[0].children.push(new TreeNode('five'))
+  tree._root.children[0].children[0].parent = tree._root.children[0]
+
+  tree._root.children[0].children.push(new TreeNode('six'))
+  tree._root.children[0].children[1].parent = tree._root.children[0]
+
+  tree._root.children[1].children.push(new TreeNode('seven'))
+  tree._root.children[1].children[0].parent = tree._root.children[1]
+
+  expect(tree._root.val).toBe('one')
+  expect(tree._root.children[0].val).toBe('two')
+  expect(tree._root.children[1].val).toBe('three')
+  expect(tree._root.children[2].val).toBe('four')
+  expect(tree._root.children[0].children[0].val).toBe('five')
+  expect(tree._root.children[0].children[1].val).toBe('six')
+  expect(tree._root.children[1].children[0].val).toBe('seven')
+  expect(tree._root.parent).toBe(null)
+
+  const df: any[] = []
+  tree.traverseDF((treeNode: any) => df.push(treeNode.val))
+  expect(df).toEqual(['five', 'six', 'two', 'seven', 'three', 'four', 'one'])
 })
--- src/_dataStructure/_tree/index.ts
+++ src/_dataStructure/_tree/index.ts
@@ -4,3 +4,15 @@ export default function Tree(data: any) {
   var node = new TreeNode(data)
   this._root = node
 }
+
+// Depth-First Search
+Tree.prototype.traverseDF = function(callback: any) {
+  recurse(this._root)
+
+  function recurse(currentNode: any) {
+    for (let i = 0; i < currentNode.children.length; i++) {
+      recurse(currentNode.children[i])
+    }
+    callback(currentNode)
+  }
+}
