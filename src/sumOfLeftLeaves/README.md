78293d0ebd0be11a01cdc7fb5c71141743a151b3
tangweikun <819573105@qq.com>
2018-02-28 18:37:17 +0800

    :sparkles: Sum of Left Leaves

---
+++ src/sumOfLeftLeaves/README.md
@@ -0,0 +1,9 @@
+# 144.Sum of Left Leaves
+
+## Description
+
+Find the sum of all left leaves in a given binary tree.
+
+## From
+
+[LeetCode](https://leetcode.com/problems/sum-of-left-leaves)
---
+++ src/sumOfLeftLeaves/index.ts
@@ -0,0 +1,10 @@
+export function sumOfLeftLeaves(root: any): any {
+  let res = 0
+
+  if (root) {
+    if (root.left && !root.left.left && !root.left.right) res += root.left.val
+    return res + sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right)
+  }
+
+  return res
+}
