b626d7e5f8b918059b75ff95b85ab02fefcd0174
tangweikun <819573105@qq.com>
2018-02-28 11:19:15 +0800

    :white_check_mark: Update test case

--- src/_dataStructure/_tree/__tests__/index.test.ts
+++ src/_dataStructure/_tree/__tests__/index.test.ts
@@ -1,31 +1,26 @@
 import Tree from '..'
 import { TreeNode } from '../treeNode'
 
-test('tree-1', () => {
-  const tree1 = new Tree('CEO')
-  expect(tree1._root).toEqual({ val: 'CEO', parent: null, children: [] })
-})
+const tree = new Tree('one')
+tree._root.children.push(new TreeNode('two'))
+tree._root.children[0].parent = tree
 
-test('tree-2', () => {
-  const tree = new Tree('one')
-  tree._root.children.push(new TreeNode('two'))
-  tree._root.children[0].parent = tree
+tree._root.children.push(new TreeNode('three'))
+tree._root.children[1].parent = tree
 
-  tree._root.children.push(new TreeNode('three'))
-  tree._root.children[1].parent = tree
+tree._root.children.push(new TreeNode('four'))
+tree._root.children[2].parent = tree
 
-  tree._root.children.push(new TreeNode('four'))
-  tree._root.children[2].parent = tree
+tree._root.children[0].children.push(new TreeNode('five'))
+tree._root.children[0].children[0].parent = tree._root.children[0]
 
-  tree._root.children[0].children.push(new TreeNode('five'))
-  tree._root.children[0].children[0].parent = tree._root.children[0]
+tree._root.children[0].children.push(new TreeNode('six'))
+tree._root.children[0].children[1].parent = tree._root.children[0]
 
-  tree._root.children[0].children.push(new TreeNode('six'))
-  tree._root.children[0].children[1].parent = tree._root.children[0]
+tree._root.children[1].children.push(new TreeNode('seven'))
+tree._root.children[1].children[0].parent = tree._root.children[1]
 
-  tree._root.children[1].children.push(new TreeNode('seven'))
-  tree._root.children[1].children[0].parent = tree._root.children[1]
-
+test('tree-initial', () => {
   expect(tree._root.val).toBe('one')
   expect(tree._root.children[0].val).toBe('two')
   expect(tree._root.children[1].val).toBe('three')
@@ -34,15 +29,21 @@ test('tree-2', () => {
   expect(tree._root.children[0].children[1].val).toBe('six')
   expect(tree._root.children[1].children[0].val).toBe('seven')
   expect(tree._root.parent).toBe(null)
+})
 
+test('tree-df', () => {
   const df: any[] = []
   tree.traverseDF((treeNode: any) => df.push(treeNode.val))
   expect(df).toEqual(['five', 'six', 'two', 'seven', 'three', 'four', 'one'])
+})
 
+test('tree-bf', () => {
   const bf: any[] = []
   tree.traverseBF((treeNode: any) => bf.push(treeNode.val))
   expect(bf).toEqual(['one', 'two', 'three', 'four', 'five', 'six', 'seven'])
+})
 
+test('tree-contains', () => {
   expect(tree.contains('four')).toBe(true)
   expect(tree.contains('zero')).toBe(false)
 })
