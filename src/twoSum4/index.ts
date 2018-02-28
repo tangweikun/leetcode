6dec41b5f8dbc42f3e653e9688957bb492a315f8
tangweikun <819573105@qq.com>
2018-02-28 17:23:03 +0800

    :hammer: Refactor two sum 4

--- src/twoSum4/index.ts
+++ src/twoSum4/index.ts
@@ -1,17 +1,13 @@
 export function findTarget(root: any, k: number) {
   const HASH = new Set()
-  let res = false
-  recurse(root)
 
-  function recurse(currentNode: any) {
-    if (!currentNode || res) return
+  function recurse(currentNode: any): any {
+    if (!currentNode) return false
+    if (HASH.has(currentNode.val)) return true
 
-    if (HASH.has(currentNode.val)) res = true
-    else HASH.add(k - currentNode.val)
-
-    recurse(currentNode.left)
-    recurse(currentNode.right)
+    HASH.add(k - currentNode.val)
+    return recurse(currentNode.left) || recurse(currentNode.right)
   }
 
-  return res
+  return recurse(root)
 }
