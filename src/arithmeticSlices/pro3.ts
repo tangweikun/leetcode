ac14cbdf6ce189143d3303726f54d56f99205662
tangweikun <819573105@qq.com>
2018-03-10 14:03:57 +0800

    :sparkles: Arithmetic Slices DP solution

--- src/arithmeticSlices/pro3.ts
+++ src/arithmeticSlices/pro3.ts
@@ -1,16 +1,17 @@
 // Time complexity : O(n). We traverse over the given A array with n elements once only.
-// Space complexity : O(n). 1-D dp of size n is used.
+// Space complexity : O(1). Constant extra space is used.
 // DP
 // HELP:
-
 export const numberOfArithmeticSlices = (nums: number[]) => {
-  let dp = Array(nums.length).fill(0)
+  let dp = 0
   let sum = 0
 
-  for (let i = 2; i < dp.length; i++) {
+  for (let i = 2; i < nums.length; i++) {
     if (nums[i] + nums[i - 2] === 2 * nums[i - 1]) {
-      dp[i] = 1 + dp[i - 1]
-      sum += dp[i]
+      dp = 1 + dp
+      sum += dp
+    } else {
+      dp = 0
     }
   }
 
