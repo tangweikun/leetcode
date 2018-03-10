cd98150f61f4b77d833a8e0d60870ec2f1ef5499
tangweikun <819573105@qq.com>
2018-03-10 14:09:31 +0800

    :sparkles: Arithmetic Slices Formula solution

---
+++ src/arithmeticSlices/pro4.ts
@@ -0,0 +1,19 @@
+// Time complexity : O(n). We iterate over A with n elements exactly once.
+// Space complexity : O(1)
+// Formula
+
+export const numberOfArithmeticSlices = (nums: number[]) => {
+  let count = 0
+  let sum = 0
+
+  for (let i = 2; i < nums.length; i++) {
+    if (nums[i] + nums[i - 2] === 2 * nums[i - 1]) {
+      count++
+    } else {
+      sum += (count + 1) * count / 2
+      count = 0
+    }
+  }
+
+  return sum + (count + 1) * count / 2
+}
