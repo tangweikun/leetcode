c5cd8923ef93d2ef4ea3903e6c0462858cb6c3b6
tangweikun <819573105@qq.com>
2018-03-25 16:55:00 +0800

    :sparkles: Binary Tree Paths pro

---
+++ src/binaryTreePaths/pro.ts
@@ -0,0 +1,21 @@
+import { I_TreeNode } from '../_interface'
+// Depth-First-Search
+// HELP:
+
+//leaf means no left and no right
+export const binaryTreePaths = function(root: I_TreeNode) {
+  const paths: any[] = []
+  const stack: number[] = []
+  dfs(root)
+  return paths.map(path => path.join('->'))
+
+  function dfs(rootNode: I_TreeNode) {
+    if (!rootNode) return
+    stack.push(rootNode.val)
+    if (rootNode.left != null) dfs(rootNode.left)
+    if (rootNode.right != null) dfs(rootNode.right)
+    //Hit a leaf!
+    if (!rootNode.right && !rootNode.left) paths.push([...stack])
+    stack.pop()
+  }
+}
