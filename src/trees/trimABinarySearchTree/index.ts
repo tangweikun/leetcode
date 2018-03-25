746d379641dd1d9e9916e926a19f04b596923457
tangweikun <819573105@qq.com>
2018-03-25 22:47:48 +0800

    :truck: Move trimABinarySearchTree --> trees

--- src/trees/trimABinarySearchTree/index.ts
+++ src/trees/trimABinarySearchTree/index.ts
@@ -1,8 +1,9 @@
+import { I_TreeNode } from './../../_interface/index'
 // HELP:
 // Time Complexity: O(n)
 // Space Complexity: O(n)
 
-export function trimBST(root: any, L: number, R: number): any {
+export function trimBST(root: I_TreeNode, L: number, R: number): I_TreeNode {
   if (root == null) return root
   if (root.val > R) return trimBST(root.left, L, R)
   if (root.val < L) return trimBST(root.right, L, R)
---
+++ src/trees/trimABinarySearchTree/test.ts
@@ -0,0 +1,42 @@
+import { TREE_1, TREE_3, TREE_4, TREE_5, TREE_7 } from './../../_tree/index'
+import { trimBST } from '.'
+
+test('trimABinarySearchTree-1', () => {
+  const expectedTree = {
+    left: { left: null, right: null, val: 2 },
+    right: null,
+    val: 1,
+  }
+  expect(trimBST(TREE_1, 1, 2)).toEqual(expectedTree)
+})
+
+test('trimABinarySearchTree-2', () => {
+  const expectedTree = {
+    left: { left: null, right: null, val: 3 },
+    right: { left: null, right: null, val: 1 },
+    val: 2,
+  }
+  expect(trimBST(TREE_3, 1, 3)).toEqual(expectedTree)
+})
+
+test('trimABinarySearchTree-3', () => {
+  const expectedTree = {
+    left: { left: { left: null, right: null, val: 4 }, right: null, val: 2 },
+    right: { left: null, right: null, val: 4 },
+    val: 3,
+  }
+  expect(trimBST(TREE_4, 2, 4)).toEqual(expectedTree)
+})
+
+test('trimABinarySearchTree-4', () => {
+  expect(trimBST(TREE_5, 1, 2)).toEqual(null)
+})
+
+test('trimABinarySearchTree-5', () => {
+  const expectedTree = {
+    left: { left: null, right: null, val: 2 },
+    right: null,
+    val: 1,
+  }
+  expect(trimBST(TREE_7, 1, 2)).toEqual(expectedTree)
+})
