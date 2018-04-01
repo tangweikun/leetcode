b7421e272776a5835bf03520b4e375ddaad97b80
tangweikun <819573105@qq.com>
2018-04-01 17:36:58 +0800

    :hammer: Refactor PathSum 2

--- src/trees/pathSum2/index.ts
+++ src/trees/pathSum2/index.ts
@@ -2,21 +2,18 @@ import { I_TreeNode } from '../../_interface'
 
 export const pathSum2 = (root: I_TreeNode, sum: number) => {
   let res: number[][] = []
-  dfs(root, [])
+  dfs(root, [], sum)
   return res
 
-  function dfs(node: I_TreeNode, prePath: number[]) {
+  function dfs(node: I_TreeNode, prePath: number[], target: number) {
     if (node) {
       const path = [...prePath, node.val]
-      if (!node.left && !node.right) {
-        const sumOfPath = path.reduce((acc, x) => acc + x)
-        if (sumOfPath === sum) res.push(path)
 
-        return
+      if (!node.left && !node.right && node.val === target) {
+        res.push(path)
       }
-
-      dfs(node.left, path)
-      dfs(node.right, path)
+      dfs(node.left, path, target - node.val)
+      dfs(node.right, path, target - node.val)
     }
   }
 }
