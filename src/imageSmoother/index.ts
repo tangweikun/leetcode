2f51a810fb07ae6c8513ef13706cc26e34aa0003
tangweikun <819573105@qq.com>
2018-04-05 13:43:21 +0800

    :hammer: Refactor Image Smoother

--- src/imageSmoother/index.ts
+++ src/imageSmoother/index.ts
@@ -1,27 +1,20 @@
 export const imageSmoother = function(M: number[][]) {
   const R = M.length
   const C = M[0].length
-  let res = Array()
+  const res = Array.from({ length: R }, () => Array(C).fill(0))
 
-  for (let i = 0; i < R; i++) {
-    res[i] = Array()
-    for (let j = 0; j < C; j++) {
-      res[i][j] = 0
-    }
-  }
-
-  for (let r = 0; r < R; ++r) {
-    for (let c = 0; c < C; ++c) {
-      let count = 0
-      for (let nr = r - 1; nr <= r + 1; ++nr) {
-        for (let nc = c - 1; nc <= c + 1; ++nc) {
+  for (let r = 0; r < R; r++) {
+    for (let c = 0; c < C; c++) {
+      let counter = 0
+      for (let nr = r - 1; nr <= r + 1; nr++) {
+        for (let nc = c - 1; nc <= c + 1; nc++) {
           if (0 <= nr && nr < R && 0 <= nc && nc < C) {
             res[r][c] += M[nr][nc]
-            count++
+            counter++
           }
         }
       }
-      res[r][c] = Math.floor(res[r][c] / count)
+      res[r][c] = Math.floor(res[r][c] / counter)
     }
   }
 
