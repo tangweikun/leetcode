52059d7427c28ca991bde7298b946ac54eb02d96
tangweikun <819573105@qq.com>
2018-05-15 12:37:56 +0800

    :sparkles: Count Numbers with Unique Digits

---
+++ src/countNumbersWithUniqueDigits/README.md
@@ -0,0 +1,12 @@
+# 215.Count Numbers with Unique Digits
+
+## Description
+
+Given a non-negative integer n, count all numbers with unique digits, x, where 0 ≤ x < 10n.
+
+Example:
+Given n = 2, return 91. (The answer should be the total numbers in the range of 0 ≤ x < 100, excluding [11,22,33,44,55,66,77,88,99])
+
+## From
+
+[LeetCode](https://leetcode.com/problems/count-numbers-with-unique-digits)
---
+++ src/countNumbersWithUniqueDigits/test.ts
@@ -0,0 +1,17 @@
+import { countNumbersWithUniqueDigits } from '.'
+
+test('Count Numbers with Unique Digits', () => {
+  expect(countNumbersWithUniqueDigits(0)).toBe(1)
+})
+
+test('Count Numbers with Unique Digits', () => {
+  expect(countNumbersWithUniqueDigits(1)).toBe(10)
+})
+
+test('Count Numbers with Unique Digits', () => {
+  expect(countNumbersWithUniqueDigits(2)).toBe(91)
+})
+
+test('Count Numbers with Unique Digits', () => {
+  expect(countNumbersWithUniqueDigits(3)).toBe(739)
+})
