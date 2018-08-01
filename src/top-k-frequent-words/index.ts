660f413db7a97e81fdfa45893f92aceb5f25f969
tangweikun <819573105@qq.com>
2018-08-01 13:59:32 +0800

    :zap: Make topKFrequent better

--- src/top-k-frequent-words/index.ts
+++ src/top-k-frequent-words/index.ts
@@ -1,12 +1,10 @@
 export const topKFrequent = (words: string[], k: number) => {
-  let hash: any = {}
-
-  for (let n of words) {
-    hash[n] = ~~hash[n] + 1
+  let count: any = {}
+  for (let w of words) {
+    count[w] = ~~count[w] + 1
   }
 
-  const entries = Object.entries(hash)
-  entries.sort((a, b) => b[1] - a[1] || a[0].localeCompare(b[0]))
-
-  return entries.slice(0, k).map(arr => arr[0])
+  return Object.keys(count)
+    .sort((a, b) => count[b] - count[a] || a.localeCompare(b))
+    .slice(0, k)
 }
