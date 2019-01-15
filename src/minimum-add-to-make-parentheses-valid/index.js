a2f1dd9972978d425baa0c11c03b29e07425eede
tangweikun <819573105@qq.com>
2019-01-15 14:12:59 +0800

    :sparkles: Minimum add to make parentheses valid

---
+++ src/minimum-add-to-make-parentheses-valid/index.js
@@ -0,0 +1,16 @@
+function minAddToMakeValid(S) {
+  let left = 0;
+  let res = 0;
+
+  for (let char of S) {
+    if (char === '(') {
+      left++;
+    } else if (left > 0) {
+      left--;
+    } else {
+      res++;
+    }
+  }
+
+  return res + left;
+}
