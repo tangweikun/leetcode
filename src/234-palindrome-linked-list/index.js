4564db4bf85be98d8de67de8ffff72cc1a882eb3
tangweikun <819573105@qq.com>
2019-09-17 19:42:24 +0800

    :sparkles: palindrome linked list

---
+++ src/234-palindrome-linked-list/index.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+  let arr = [];
+  while (head) {
+    arr.push(head.val);
+    head = head.next;
+  }
+
+  for (let i = 0; i < arr.length / 2; i++) {
+    if (arr[i] !== arr[arr.length - 1 - i]) return false;
+  }
+  return true;
+};
