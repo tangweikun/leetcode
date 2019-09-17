61ff46bf52397bfd8cc501eb5a93abf03e8cfe63
tangweikun <819573105@qq.com>
2019-09-17 19:14:37 +0800

    :sparkles: remove linked list elements

---
+++ src/203-remove-linked-list-elements/index.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function(head, val) {
+  var cur = head;
+  var pre = null;
+  while (cur) {
+    if (cur.val == val) {
+      if (pre) {
+        pre.next = cur.next;
+      } else {
+        head = cur.next;
+        pre = null;
+      }
+    } else {
+      pre = cur;
+    }
+
+    cur = cur.next;
+  }
+  return head;
+};
