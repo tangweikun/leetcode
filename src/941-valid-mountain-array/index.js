8b4c003380cf17f1e254ca2ee2d9912f24868a9c
tangweikun <819573105@qq.com>
2019-10-29 17:19:14 +0800

    :sparkles: valid mountain array

---
+++ src/941-valid-mountain-array/index.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} A
+ * @return {boolean}
+ */
+var validMountainArray = function(A) {
+  if (A.length < 3) return false;
+  if (A[0] >= A[1]) return false;
+
+  let isDown = false;
+  for (let i = 2; i < A.length; i++) {
+    if (A[i] === A[i - 1]) return false;
+    if (isDown) {
+      if (A[i] > A[i - 1]) return false;
+    } else {
+      if (A[i] < A[i - 1]) isDown = true;
+    }
+  }
+
+  return isDown;
+};
