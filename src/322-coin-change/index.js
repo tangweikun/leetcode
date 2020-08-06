53ec5eb515494f8bb3cbc8da10b455dc6db48a38
tangweikun <819573105@qq.com>
2020-08-06 09:25:13 +0800

    :sparkles: coin change

---
+++ src/322-coin-change/index.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function (coins, amount) {
+  const dp = Array(amount + 1).fill(Infinity);
+  dp[0] = 0;
+
+  for (let i = 1; i < amount + 1; i++) {
+    for (let j = 0; j < coins.length; j++) {
+      if (coins[j] <= i) {
+        dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+      }
+    }
+  }
+
+  return dp[amount] === Infinity ? -1 : dp[amount];
+};
