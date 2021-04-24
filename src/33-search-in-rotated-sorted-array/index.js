0c4bbeff1a44a52735489e93e88e99fb462760bb
tangweikun <819573105@qq.com>
2021-04-24 11:46:44 +0800

    :sparkles: search in rotated sorted array

--- src/33-search-in-rotated-sorted-array/index.js
+++ src/33-search-in-rotated-sorted-array/index.js
@@ -1,31 +1,36 @@
-/**
+/*
  * @param {number[]} nums
- * @return {void} Do not return anything, modify nums in-place instead.
+ * @param {number} target
+ * @return {number}
  */
-function nextPermutation(nums) {
-  let i = nums.length - 2; // 向左遍历，i从倒数第二开始是为了nums[i+1]要存在
-  while (i >= 0 && nums[i] >= nums[i + 1]) {
-    // 寻找第一个小于右邻居的数
-    i--;
-  }
+var search = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
 
-  if (i >= 0) {
-    // 这个数在数组中存在，从它身后挑一个数，和它换
-    let j = nums.length - 1; // 从最后一项，向左遍历
-    while (j >= 0 && nums[j] <= nums[i]) {
-      // 寻找第一个大于 nums[i] 的数
-      j--;
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (nums[mid] === target) {
+      return mid;
     }
-    [nums[i], nums[j]] = [nums[j], nums[i]]; // 两数交换，实现变大
+
+    if (nums[mid] < nums[right]) {
+      if (nums[mid] < target && target <= nums[right]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    } else {
+      if (nums[mid] > target && target >= nums[left]) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    }
   }
 
-  // 如果 i = -1，说明是递减排列，如 3 2 1，没有下一排列，直接翻转为最小排列：1 2 3
-  let l = i + 1;
-  let r = nums.length - 1;
-  while (l < r) {
-    // i 右边的数进行翻转，使得变大的幅度小一些
-    [nums[l], nums[r]] = [nums[r], nums[l]];
-    l++;
-    r--;
-  }
-}
+  return -1;
+};
+
+const assert = require('assert').strict;
+
+assert.equal(search([4, 5, 6, 7, 0, 1, 2], 0), 4);
